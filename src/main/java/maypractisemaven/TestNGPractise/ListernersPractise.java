a1723dbdc34933ab39673b424e8e688ce25b03d5
Rajath <rajath.vp@gmail.com>
2019-06-10 11:07:24 -0700

    this is my scond commit

---
+++ src/main/java/maypractisemaven/TestNGPractise/ListernersPractise.java
@@ -0,0 +1,10 @@
+package maypractisemaven.TestNGPractise;
+
+public class ListernersPractise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println(" Hi i am trying to learn Listerns ");
+	}
+
+}
